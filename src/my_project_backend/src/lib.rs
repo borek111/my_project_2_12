b23fa5c05d99e8fe2f89757ef5ee916b3454dd0e
Przemysław Grabecki <przemekgrabecki@gmail.com>
2024-12-02 17:56:15 +0000

    p

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,3 +1,23 @@
+use std::cell::RefCell;
+
+thread_local!{
+    static MSG: RefCell<String> = RefCell::new(String::new());
+}
+
+#[ic_cdk::query]
+fn get_msg() -> String {
+    MSG.with(|msg| msg.borrow().clone())
+}
+
+
+
+#[ic_cdk::query]
+fn set_msg(new_msg: String) {
+    MSG.with(|msg| *msg.borrow_mut() =new_msg)
+}
+//COUNTER.with(|count| *count.borrow_mut() = n);
+
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
